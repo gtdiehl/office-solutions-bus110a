c7963cb65ce9ab3b5a15c0a34cd06bcaefbe8c1d
Greg <34967045+gtdiehl@users.noreply.github.com>
2019-10-10 13:42:00 -0700

    Renamed objects to conform to PEP8

--- app/database.py
+++ app/database.py
@@ -1,19 +1,20 @@
 import sqlite3
 
+
 class Database:
     def __init__(self):
-        self._UserDBName = 'OS_Employee.db'
+        self._user_db_name = 'OS_Employee.db'
 
-    def _connectUserDB(self):
+    def _connect_user_db(self):
         # Connect to the Database
         try:
-            self.dbConn = sqlite3.connect(self._UserDBName)
+            self.db_conn = sqlite3.connect(self._user_db_name)
 
             # This statement makes the ResultSet returned as a List rather than a Tuple
             # that needs to be parsed
-            self.dbConn.row_factory = lambda cursor, row: row[0]
+            self.db_conn.row_factory = lambda cursor, row: row[0]
 
-            if self.dbConn != None:
+            if self.db_conn is not None:
                 return True
             else:
                 return False
@@ -22,32 +23,32 @@ class Database:
             print(e)
             return False
 
-    def _disconnectUserDB(self):
-        self.dbConn.close()
+    def _disconnect_user_db(self):
+        self.db_conn.close()
 
-    def queryUserDB(self, queryStatement):
+    def query_user_db(self, query_statement):
         try:
-            self._connectUserDB()
-            cur = self.dbConn.cursor()
-            cur.execute(queryStatement)
+            self._connect_user_db()
+            cur = self.db_conn.cursor()
+            cur.execute(query_statement)
             results = cur.fetchall()
-            self._disconnectUserDB()
+            self._disconnect_user_db()
         except sqlite3.Error as e:
             results = None
             print(e)
         return results
 
-    def insertUserDB(self, insertStatement):
+    def insert_user_db(self, insert_statement):
         try:
             self._connectUserDB()
-            cur = self.dbConn.cursor()
-            cur.execute(insertStatement)
-            self.dbConn.commit()
-            self._disconnectUserDB()
+            cur = self.db_conn.cursor()
+            cur.execute(insert_statement)
+            self.db_conn.commit()
+            self._disconnect_user_db()
             return True
         except sqlite3.Error as e:
             print(e)
             return False
 
-    def deleteUserDB(self, delete):
+    def delete_user_db(self, delete):
         pass
