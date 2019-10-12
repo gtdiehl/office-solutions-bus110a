ef940f075d3c3e55ad9da88d078b57c106458406
Greg <34967045+gtdiehl@users.noreply.github.com>
2019-10-11 21:51:26 -0700

    Initial commit for conftest.py

---
+++ tests/conftest.py
@@ -0,0 +1,27 @@
+import pytest
+import sqlite3
+
+
+@pytest.fixture(scope="session")
+def setup_db(tmpdir_factory):
+    file = tmpdir_factory.mktemp("data").join("OS_Employee.db")
+    conn = sqlite3.connect(file)
+    conn.execute("CREATE TABLE Employee (EmployeeID, FirstName, LastName, Email, Password)")
+    conn.execute("INSERT INTO \"Employee\" VALUES (\'1005\',\'Nora\',\'Chang\',\'nora.chang@gmail.com\',\'nora\');")
+    conn.execute("INSERT INTO \"Employee\" VALUES (\'1006\',\'Elanor\',\'White\',\'elanor.white@gmail.com\'" +
+                 ",\'elanor\');")
+    conn.execute("INSERT INTO \"Employee\" VALUES (\'1007\',\'Charles\',\'Lopez\',\'charles.lopez@gmail.com\'," +
+                 "\'charles\');")
+    conn.execute("INSERT INTO \"Employee\" VALUES (\'1009\',\'Emma\',\'Moran\',\'emma.moran@gmail.com\',\'emma\');")
+    conn.execute("INSERT INTO \"Employee\" VALUES (\'1008\',\'Patrick\',\'Harris\',\'patrick.harris@gmail.com\'," +
+                 "\'patrick\');")
+    conn.execute("INSERT INTO \"Employee\" VALUES (\'1003\',\'William\',\'Jones\',\'william.jones@gmail.com\'," +
+                 "\'william\');")
+    conn.execute("INSERT INTO \"Employee\" VALUES (\'1004\',\'Jennifer\',\'Lee\',\'jennifer.lee@gmail.com\'," +
+                 "\'jennifer\');")
+    conn.execute("INSERT INTO \"Employee\" VALUES (\'1002\',\'Belle\',\'Nguyen\',\'belle.nguyen@gmail.com\'," +
+                 "\'belle\');")
+    conn.execute("INSERT INTO \"Employee\" VALUES (\'1001\',\'Bob\',\'Moore\',\'bob.moore@gmail.com\',\'bob\');")
+    conn.commit()
+    yield file
+    conn.close()
