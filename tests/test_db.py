c9c85374f378da4e79836e72940a2176d86c04d5
Greg <34967045+gtdiehl@users.noreply.github.com>
2019-10-11 21:50:15 -0700

    Updated unit tests for Database class

--- tests/test_db.py
+++ tests/test_db.py
@@ -2,31 +2,30 @@ from app.database import Database
 import pytest
 
 
-def test_db_connect():
-    db = Database()
-    assert db._connect_user_db() == True
+@pytest.mark.usefixtures("setup_db")
+class TestDB:
+    def test_db_connect(self, setup_db):
+        test_db = Database(setup_db)
+        assert test_db._connect_user_db() == True
 
+    def test_db_disconnect(self, setup_db):
+        pass
 
-def test_db_disconnect():
-    pass
+    @pytest.mark.parametrize("test_input,expected",
+                             [("SELECT * FROM Employee", 9),
+                              ("SELECT Email from Employee WHERE Email is 'nora.chang@gmail.com'", 1)])
+    def test_query_user_db(self, setup_db, test_input, expected):
+        test_db = Database(setup_db)
+        results = test_db.query_user_db(test_input)
+        assert len(results) == expected
 
-
-@pytest.mark.parametrize("test_input,expected",
-                         [("SELECT * FROM Employee", 9),
-                          ("SELECT Email from Employee WHERE Email is 'nora.chang@gmail.com'", 1)])
-def test_query_user_db(test_input, expected):
-    db = Database()
-    results = db.query_user_db(test_input)
-    assert len(results) == expected
-
-
-@pytest.mark.parametrize("test_input,expected",
-                        [("INSERT INTO Employee (EmployeeID, FirstName, LastName, Email, Password) " +
-                          "VALUES (9999,'Py','Tester','py.tester@gmail.com','py')", []),
-                         ("INSERT INTO Employee (EmployeeID, FirstName, LastName, Email) " +
-                          "VALUES (9999,'Py','Tester','py.tester@gmail.com','py')", None)
-                         ])
-def test_insert_user_db(test_input, expected):
-    db = Database()
-    results = db.query_user_db(test_input)
-    assert results == expected
+    @pytest.mark.parametrize("test_input,expected",
+                             [("INSERT INTO Employee (EmployeeID, FirstName, LastName, Email, Password) " +
+                              "VALUES (9999,'Py','Tester','py.tester@gmail.com','py')", []),
+                              ("INSERT INTO Employee (EmployeeID, FirstName, LastName, Email) " +
+                              "VALUES (9999,'Py','Tester','py.tester@gmail.com','py')", None)
+                              ])
+    def test_insert_user_db(self, setup_db, test_input, expected):
+        db = Database(setup_db)
+        results = db.query_user_db(test_input)
+        assert results == expected
