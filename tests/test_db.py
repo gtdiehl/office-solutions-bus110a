cb9747e2ef86cb6467795b39ea6c7cdd7e03b7e2
Greg <34967045+gtdiehl@users.noreply.github.com>
2019-10-16 16:36:36 -0700

    Added more tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -19,13 +19,36 @@ class TestDB:
         results = test_db.query_user_db(test_input)
         assert len(results) == expected
 
+    @pytest.mark.parametrize("test_input",
+                             ["SELECT * FROM NoTable"])
+    def test_query_exception(self, setup_db, test_input):
+        test_db = Database(setup_db)
+        with pytest.raises(Exception):
+            assert test_db.query_user_db(test_input)
+
     @pytest.mark.parametrize("test_input,expected",
                              [("INSERT INTO Employee (EmployeeID, FirstName, LastName, Email, Password) " +
-                              "VALUES (9999,'Py','Tester','py.tester@gmail.com','py')", []),
-                              ("INSERT INTO Employee (EmployeeID, FirstName, LastName, Email) " +
-                              "VALUES (9999,'Py','Tester','py.tester@gmail.com','py')", None)
+                              "VALUES (9999,'Py','Tester','py.tester@gmail.com','py')", True)
                               ])
     def test_insert_user_db(self, setup_db, test_input, expected):
         db = Database(setup_db)
-        results = db.query_user_db(test_input)
+        results = db.insert_user_db(test_input)
+        assert results == expected
+
+    @pytest.mark.parametrize("test_input",
+                             [("INSERT INTO Employee (EmployeeID, FirstName, LastName, Email) " +
+                              "VALUES (9999,'Py','Tester','py.tester@gmail.com','py')")
+                              ])
+    def test_insert_exception(self, setup_db, test_input):
+        db = Database(setup_db)
+        with pytest.raises(Exception):
+            assert db.insert_user_db(test_input)
+
+    @pytest.mark.parametrize("test_input,expected",
+                             [("DELETE FROM Employee WHERE EMail is 'kk.chang@gmail.com'", False),
+                              ("DELETE FROM Employee WHERE EMail is 'nora.chang@gmail.com'", True),
+                              ("DELETE FROM Employee", True)])
+    def test_delete_user_db(self, setup_db, test_input, expected):
+        db = Database(setup_db)
+        results = db.delete_user_db(test_input)
         assert results == expected
