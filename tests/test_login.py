a1e2a7aa937079a1312e7b495b45cd84c1362d71
Greg <34967045+gtdiehl@users.noreply.github.com>
2019-10-16 22:05:40 -0700

    Implementing test_login_success method

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,9 +1,21 @@
+from app.database import Database
+from app.loginfunction import login
+from tests.input_test_base import set_keyboard_input, get_display_output
 import pytest
 
 @pytest.mark.usefixtures("setup_db")
 class TestLogin:
     def test_login_success(self, setup_db):
-        pass
+        test_db = Database(setup_db)
+
+        set_keyboard_input(["nora.chang@gmail.com", "nora"])
+
+        login(test_db).login()
+
+        output = get_display_output()
+
+        assert output == ["Please enter email: ", "Please enter password: "]
+
 
     def test_login_failure(self, setup_db):
         pass
