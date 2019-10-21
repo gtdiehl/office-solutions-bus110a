2057bf79720c4e00a9bfa09c3787473a6fd26bc5
Greg <34967045+gtdiehl@users.noreply.github.com>
2019-10-21 09:19:17 -0700

    Update main.py
    Added check for return from login.  If true launch the menu

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 import database
 import loginfunction
+import menu_module
 
 
 def main():
     mydb = database.Database()
     mylogin = loginfunction.login(mydb)
 
-    mylogin.login()
+    if mylogin.login() is True:
+        BestMenu.createBestMenu()
 
 if __name__ == "__main__":
     main()
