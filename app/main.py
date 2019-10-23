89a3c5cd4ec6e96d0403f3096ac65d503864eebf
Greg <34967045+gtdiehl@users.noreply.github.com>
2019-10-22 18:25:59 -0700

    Added menu to match the FlowChart and GUI Mockup.  The user can either create a new user or login with an existing user.

--- app/main.py
+++ app/main.py
@@ -2,13 +2,37 @@ import database
 import loginfunction
 import menu_module
 
+mydb = database.Database()
+mylogin = loginfunction.login(mydb)
+
 
 def main():
-    mydb = database.Database()
-    mylogin = loginfunction.login(mydb)
+    ans = True
+    while ans:
+        ans = _menuloop()
 
-    if mylogin.login() is True:
-        menu_module.BestMenu(mydb).createBestMenu()
+
+def _menuloop():
+    print("Office Solutions Corporation\nSales Reporting Application v1.0")
+    print("===============================================")
+    print("[1] Login with E-Mail address")
+    print("[2] Register a New User")
+    print("===============================================")
+    menu_selection = input("Please enter an option [1-2, q to Quit] : ")
+    if menu_selection == "1":
+        pass
+    elif menu_selection == "2":
+        if mylogin.login() is True:
+            return menu_module.BestMenu(mydb).createBestMenu()
+    elif menu_selection == "q" or menu_selection == "Q":
+        print("\nGoodbye!")
+        return False
+    elif menu_selection == "":
+        print("\nNot a valid choice. Please try again.")
+    else:
+        print("\nNot a valid choice. Please try again.")
+    return True
+
 
 if __name__ == "__main__":
     main()
