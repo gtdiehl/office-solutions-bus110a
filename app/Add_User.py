1a762e577a4cea721891bcbd8ad9fe3e10541e32
Greg <34967045+gtdiehl@users.noreply.github.com>
2019-10-23 19:36:55 -0700

    Added email_validator library and max length of password

--- app/Add_User.py
+++ app/Add_User.py
@@ -1,4 +1,5 @@
 import re
+from email_validator import validate_email, EmailNotValidError
 
 
 class UserController:
@@ -19,7 +20,7 @@ class UserController:
     def _verifyPassword(self, password):
         # Password can only consist of at least 8 characters
         # only limited to letters and numbers.
-        if re.match(r'[a-zA-Z0-9]{8,}', password) is not None:
+        if re.match(r'[a-zA-Z0-9]{8,12}', password) is not None:
             return True
         else:
             return False
@@ -29,10 +30,20 @@ class UserController:
         # If the e-mail address is not in the proper format the function
         # will return False
         if re.search(r'(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)', address) is not None:
-            return True
+            pass
         else:
+            print("The e-mail address must be in user@domain.com format. Please enter a valid e-Mail address.")
             return False
-    
+
+        try:
+            v = validate_email(address)  # validate and get info
+            email = v["email"]  # replace with normalized form
+            return  True
+        except EmailNotValidError as e:
+            # email is not valid, exception message is human-readable
+            print(str(e) + " Please enter in a valid domain name.")
+            return False
+
     def addNewUser(self):
         
         print("Enter new user information, all information is case-sensitive.")
@@ -67,7 +78,6 @@ class UserController:
                 print("E-Mail address can not be blank.  Please enter an e-mail address.")
                 continue
             if not self._verifyEmailAddress(newEmailAddress):
-                print("The e-mail address must be in user@domain.com format. Please enter a valid e-Mail address.")
                 continue
             else:
                 break
@@ -80,7 +90,7 @@ class UserController:
                 continue
             if not self._verifyPassword(newUserPassword):
                 print("Password does not meet requirements." +
-                      "\nMust be at least 8 characters in length, only letters and numbers are allowed" +
+                      "\nMust be at least 8 to 12 characters in length, only letters and numbers are allowed" +
                       "\nRe-enter Password")
                 continue
                 
