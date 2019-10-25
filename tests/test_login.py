b1e7e62edff14ffc5f9dd185d081a89796b0ae9d
Greg <34967045+gtdiehl@users.noreply.github.com>
2019-10-25 15:08:57 -0700

    Reworked tests to work with new messages

--- tests/test_login.py
+++ tests/test_login.py
@@ -3,24 +3,36 @@ from app.loginfunction import login
 from tests.input_test_base import set_keyboard_input, get_display_output
 import pytest
 
+
 @pytest.mark.usefixtures("setup_db")
 class TestLogin:
-    def test_login_success(self, setup_db):
+    @pytest.mark.parametrize("test_input_name, test_input_password",
+                             [("nora.chang@gmail.com", "nora"),
+                              ("elanor.white@gmail.com", "elanor")])
+    def test_login_success(self, setup_db, test_input_name, test_input_password):
         test_db = Database(setup_db)
-        set_keyboard_input(["nora.chang@gmail.com", "nora"])
+        set_keyboard_input([test_input_name, test_input_password])
         result = login(test_db).login()
         output = get_display_output()
-        assert output == ["Please enter email: ", "Please enter password: ", "Login Successful"]
+        assert output == ["Please enter email: ", "Please enter password: ", "\nLogin Successful"]
         assert result == True
 
-    def test_login_failure(self, setup_db):
+    @pytest.mark.parametrize("test_input_name_1, test_input_password_1, test_input_name_2, test_input_password_2, "
+                             "test_input_name_3, test_input_password_3",
+                             [("nora.chang@gmail.com", "no", "nora.chang@gmail.com", "no", "nora.chang@gmail.com",
+                               "no")])
+    def test_login_failure(self, setup_db, test_input_name_1, test_input_password_1, test_input_name_2,
+                           test_input_password_2, test_input_name_3, test_input_password_3):
         test_db = Database(setup_db)
-        set_keyboard_input(["nora.chang@gmail.com", "no", "nora.chang@gmail.com", "no", "nora.chang@gmail.com", "no"])
+        set_keyboard_input([test_input_name_1, test_input_password_1, test_input_name_2, test_input_password_2,
+                            test_input_name_3, test_input_password_3])
         result = login(test_db).login()
         output = get_display_output()
         assert output == ["Please enter email: ", "Please enter password: ",
-                          "Unsuccessful login attempt.  Please try again.", "Please enter email: ",
-                          "Please enter password: ", "Unsuccessful login attempt.  Please try again.",
-                          "Please enter email: ", "Please enter password: ", "Unsuccessful login attempt.",
-                          "Please contact your administrator for your login credentials."]
+                          "Unsuccessful login attempt.  Please try again. Note that e-mail addresses and passwords are "
+                          "case-sensitive.", "Please enter email: ", "Please enter password: ", "Unsuccessful login "
+                          "attempt.  Please try again. Note that e-mail addresses and passwords are case-sensitive.",
+                          "Please enter email: ", "Please enter password: ", "Unsuccessful login attempt. Note that "
+                          "e-mail addresses and passwords are case-sensitive.", "Please contact your administrator for "
+                          "your login credentials."]
         assert result == False
