322be644e62ca23ec905da17ecbd364681c1f2c9
Greg <34967045+gtdiehl@users.noreply.github.com>
2019-10-25 20:39:45 -0700

    Initial commit

---
+++ tests/test_user.py
@@ -0,0 +1,53 @@
+from app.database import Database
+from app.Add_User import UserController
+import pytest
+
+
+@pytest.mark.usefixtures("setup_db")
+class TestUser:
+    def test_add_user_success(self):
+        pass
+
+    def test_add_user_failure(self):
+        pass
+
+    @pytest.mark.parametrize("test_input,expected",
+                             [("greg", True),
+                              ("Greg", True),
+                              ("greg greg", False),
+                              ("a", True),
+                              ("098743fdsf", False),
+                              ("lkj43525", False),
+                              ("fred@@", False)])
+    def test_verify_user_name(self, setup_db, test_input, expected):
+        mydb = Database(setup_db)
+        user = UserController(mydb)
+        assert user._verifyUserName(test_input) == expected
+
+    @pytest.mark.parametrize("test_input,expected",
+                             [("1", False),
+                              ("1234567", False),
+                              ("abcdefg", False),
+                              ("12345678", True),
+                              ("mypassis__", False),
+                              ("password@@", False)])
+    def test_verify_password(self, setup_db, test_input, expected):
+        mydb = Database(setup_db)
+        user = UserController(mydb)
+        assert user._verifyPassword(test_input) == expected
+
+    @pytest.mark.parametrize("test_input,expected",
+                             [("greg@greg.com", True),
+                              ("fred@yahoo.com.br", True),
+                              ("tom@f.c", False),
+                              ("uri@jo.io", True),
+                              ("myp", False),
+                              ("@", False),
+                              ("passwor@", False)])
+    def test_verify_email_address(self, setup_db, test_input, expected):
+        mydb = Database(setup_db)
+        user = UserController(mydb)
+        assert user._verifyEmailAddress(test_input) == expected
+
+    def test_get_next_user_id(self):
+        pass
