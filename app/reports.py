5e9aacf9635c815e426ef077cffe4cb65205ac10
dole84 <56375593+dole84@users.noreply.github.com>
2019-10-28 20:34:53 -0700

    newer

--- app/reports.py
+++ app/reports.py
@@ -14,27 +14,22 @@ SalesDataFull = pd.ExcelFile("SalesDataFull.xlsx")
 OrdersOnlyData = SalesDataFull.parse("Orders")
 lines = "="*25
 
-prodcol_and_profcol = OrdersOnlyData[["Product Name","Profit"]]
-least_profit = prodcol_and_profcol.sort_values(by= "Profit")
-most_profit = prodcol_and_profcol.sort_values(by= "Profit", ascending = False)
-
-
-##
-#class ReportsMenu:
-#    def __init__(self):
-#        
-#  
-#        def top_ten_profits(self):
-print(least_profit.head(10))
+def top_ten_profits():
+            prodcol_and_profcol = OrdersOnlyData[["Product Name","Profit"]]
+            most_profit = prodcol_and_profcol.sort_values(by= "Profit", ascending = False)
+            print(most_profit.head(10))
             
-#        def least_ten_profits(self):
-print(most_profit.head(10))
-
-            
-
-       
+                
+def least_ten_profits():
+            prodcol_and_profcol = OrdersOnlyData[["Product Name","Profit"]]
+            least_profit = prodcol_and_profcol.sort_values(by= "Profit")
+            print(least_profit.head(10))          
         
-       
-#        
-#    def top_ten_least():
-#        pass
+                  
+top_ten_profits()          
+least_ten_profits()
+
+
+
+
+
