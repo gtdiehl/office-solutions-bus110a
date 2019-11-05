0be543159b8c89c0cff804017c413b0818c1b1f4
jkastle <joel.castillo@sjsu.edu>
2019-11-05 10:53:58 -0800

    Update reports.py
    Integrating Pandas_Format into reports.py

--- app/reports.py
+++ app/reports.py
@@ -9,6 +9,7 @@ Created on Mon Oct 28 14:30:32 2019
 import pandas as pd
 from datetime import date
 import numpy as np
+import Pandas_Format
 SalesDataFull = pd.ExcelFile("SalesDataFull.xlsx")
 OrdersOnlyData = SalesDataFull.parse("Orders")
 ordersinfo = OrdersOnlyData[["Order Date","Product Name","Quantity","Profit",]]
@@ -20,7 +21,7 @@ def top_ten_profits(from_month, from_year, to_month, to_year):
             fitered_df = _filter_df_by_date(prodcol_and_profcol, "Order Date", from_month, from_year, to_month, to_year)
             
             most_profit = fitered_df.sort_values(by= "Profit", ascending = False)
-            print(most_profit.head(10))
+            Pandas_Format.print_report(most_profit, 10)
 
 
 def top_ten_profit_ave(from_month, from_year, to_month, to_year):
@@ -29,13 +30,13 @@ def top_ten_profit_ave(from_month, from_year, to_month, to_year):
 
     df1["Average Profit Per Unit"] = df1.loc[:,"Profit"].apply(np.float) / df1.loc[:,"Quantity"].apply(np.float)                
     df1 = df1.sort_values(by="Average Profit Per Unit", ascending=False)
-    print(df1.head(10))
+    Pandas_Format.print_report(df1, 10)
             
                 
 def least_ten_profits(from_month, from_year, to_month, to_year):
             prodcol_and_profcol = OrdersOnlyData[["Product Name","Profit"]]
             least_profit = prodcol_and_profcol.sort_values(by= "Profit")
-            print(least_profit.head(10))          
+            Pandas_Format.print_report(least_profit, 10)
 
 
 def least_ten_profits_ave(from_month, from_year, to_month, to_year):
@@ -44,7 +45,7 @@ def least_ten_profits_ave(from_month, from_year, to_month, to_year):
 
     df1["Average Profit Per Unit"] = df1.loc[:,"Profit"].apply(np.float) / df1.loc[:,"Quantity"].apply(np.float)                
     df1 = df1.sort_values(by="Average Profit Per Unit", ascending=True)
-    print(df1.head(10))
+    Pandas_Format.print_report(df1, 10)
 
         
 def _filter_df_by_date(df, date_column, from_month, from_year, to_month, to_year):
