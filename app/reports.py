712329096e4ea4d8836e55597c04a53924ae2f3e
Greg <34967045+gtdiehl@users.noreply.github.com>
2019-11-04 21:10:46 -0800

    Reverting change and cleaned up code a bit

--- app/reports.py
+++ app/reports.py
@@ -9,13 +9,10 @@ Created on Mon Oct 28 14:30:32 2019
 import pandas as pd
 from datetime import date
 import numpy as np
-#import scipy.stats
-#import seaborn as sns
-#import matplotlib.pyplot as plt
 SalesDataFull = pd.ExcelFile("SalesDataFull.xlsx")
 OrdersOnlyData = SalesDataFull.parse("Orders")
 ordersinfo = OrdersOnlyData[["Order Date","Product Name","Quantity","Profit",]]
-lines = "="*25
+
 
 def top_ten_profits(from_month, from_year, to_month, to_year):
             prodcol_and_profcol = OrdersOnlyData[["Order Date", "Product Name","Profit"]]
@@ -25,7 +22,8 @@ def top_ten_profits(from_month, from_year, to_month, to_year):
             most_profit = fitered_df.sort_values(by= "Profit", ascending = False)
             print(most_profit.head(10))
 
-def toptenprofit_ave():
+
+def top_ten_profit_ave(from_month, from_year, to_month, to_year):
     df1 = ordersinfo[["Product Name","Quantity","Profit", 
                 "Order Date"]].groupby("Product Name").sum()
 
@@ -34,12 +32,13 @@ def toptenprofit_ave():
     print(df1.head(10))
             
                 
-def least_ten_profits():
+def least_ten_profits(from_month, from_year, to_month, to_year):
             prodcol_and_profcol = OrdersOnlyData[["Product Name","Profit"]]
             least_profit = prodcol_and_profcol.sort_values(by= "Profit")
             print(least_profit.head(10))          
-        
-def toptenleast_ave():
+
+
+def least_ten_profits_ave(from_month, from_year, to_month, to_year):
     df1 = ordersinfo[["Product Name","Quantity","Profit", 
                 "Order Date"]].groupby("Product Name").sum()
 
@@ -54,13 +53,6 @@ def _filter_df_by_date(df, date_column, from_month, from_year, to_month, to_year
         (df[date_column] < pd.Timestamp(date(to_year, to_month + 1, 1)))
         ]
     return filtered_data
-
-toptenleast_ave()
-#toptenprofit_ave()
-#least_ten_profits()
-                  
-#top_ten_profits()          
-#least_ten_profits()
 
 
 
