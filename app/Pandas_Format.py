c149adc4a8e829b56c8422295628d0b9a8a24831
Greg <34967045+gtdiehl@users.noreply.github.com>
2019-11-12 22:21:33 -0800

    Added comments to try out tabulate

--- app/Pandas_Format.py
+++ app/Pandas_Format.py
@@ -8,6 +8,7 @@ Created on Wed Oct 30 19:55:16 2019
 #Fixes currency format into USD & two decimal places
 
 import pandas as pd
+#from tabulate import tabulate
 pd.set_option('display.max_columns', None)  
 pd.set_option('display.expand_frame_repr', False)
 pd.set_option('max_colwidth', -1)
@@ -18,6 +19,7 @@ pd.set_option("display.colheader_justify","left")
 def print_report(df, rows):
     with pd.option_context('display.float_format', _formatfunc):
         print(df[:rows].to_string(index=False))
+        #print(tabulate(df.iloc[:10], headers='keys', tablefmt='psql', showindex='False'))
 
 def _formatfunc(*args, **kwargs):
     value = args[0]
