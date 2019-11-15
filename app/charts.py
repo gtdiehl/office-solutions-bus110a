aabd4c150734721a76d28622d9384651784c98c5
Greg <34967045+gtdiehl@users.noreply.github.com>
2019-11-15 13:48:27 -0800

    Update charts.py
    Added line graphs to the fourth example to compare region sales and profits to overall company sales and profits

--- app/charts.py
+++ app/charts.py
@@ -2,9 +2,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from datetime import date
 
-from matplotlib.pyplot import title
-
-
 def _filter_df_by_date(df, date_column, from_month, from_year, to_month, to_year):
     filtered_data = df[
         (df[date_column] >= pd.Timestamp(date(from_year, from_month, 1))) &
@@ -76,20 +73,52 @@ def chart_example_three():
     plt.show()
 
 def chart_example_four():
+    sales = {}
+    profits = {}
+    
     xl = pd.ExcelFile("SalesDataFull.xlsx")
     df = xl.parse("Orders")
+    
+    a = 2.5
+    for i in range(1, 12):
+        s = _filter_df_by_date(df, "Order Date", i, 2015, i, 2015)
+        sales[a] = s['Sales'].sum()
+        profits[a] = s['Profit'].sum()
+        a = a + 4
+    sales_list = sorted(sales.items())
+    profits_list = sorted(profits.items())
+    
+    x1, y1 = zip(*profits_list)
+    x2, y2 = zip(*sales_list)
+        
     df = _filter_df_by_date(df, "Order Date", 1, 2015, 1, 2016)
     df.set_index(df["Order Date"],inplace=True)
     
     df1 = df.groupby([pd.Grouper(freq='M'), 'Region'])['Profit'].sum()
     df2 = df.groupby([pd.Grouper(freq='M'), 'Region'])['Sales'].sum()
     result = pd.concat([df1, df2], axis=1, sort=False)
+ 
+    # define the number of ticks
     
     fig, ax = plt.subplots(figsize=(17, 6))
+    ax.set_title("Sales and Profits by Region per Month vs Company Total per Month")
+
+    ax.set(ylabel='Dollar Amount')
     result.plot(kind='bar', ax=ax)
+    ax.plot(x1, y1, '-o', color='#1f77b4')
+    ax.plot(x2, y2, '-o', color='#ff7f0e')
+    vals = ax.get_yticks()
+    ax.set_yticklabels(['${:,.0f}'.format(x) for x in vals])         
+    i = 0
+    for x, y in sales.items():
+        ax.text(x+0.2, y, '${:,.0f}'.format(y), horizontalalignment='left', size='medium', color='black', weight='bold')
+        i = i + 1
+    for x, y in profits.items():
+        ax.text(x+0.2, y, '${:,.0f}'.format(y), horizontalalignment='left', size='medium', color='black', weight='bold')
+        i = i + 1
     plt.show()
 
-chart_example_one()
-chart_example_two()
-chart_example_three()
+#chart_example_one()
+#chart_example_two()
+#chart_example_three()
 chart_example_four()
