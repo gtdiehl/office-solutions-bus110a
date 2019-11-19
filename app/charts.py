7ed5075be3f519786eaefde140a1bd403abfbb5d
Greg <34967045+gtdiehl@users.noreply.github.com>
2019-11-18 19:09:27 -0800

    Update charts.py

--- app/charts.py
+++ app/charts.py
@@ -101,24 +101,113 @@ def chart_example_four():
     # define the number of ticks
     
     fig, ax = plt.subplots(figsize=(17, 6))
+    ax2 = ax.twinx()
     ax.set_title("Sales and Profits by Region per Month vs Company Total per Month")
 
     ax.set(ylabel='Dollar Amount')
     result.plot(kind='bar', ax=ax)
-    ax.plot(x1, y1, '-o', color='#1f77b4')
-    ax.plot(x2, y2, '-o', color='#ff7f0e')
+    ax2.plot(x1, y1, '-o', color='#1f77b4')
+    ax2.plot(x2, y2, '-o', color='#ff7f0e')
     vals = ax.get_yticks()
-    ax.set_yticklabels(['${:,.0f}'.format(x) for x in vals])         
+    ax.set_yticklabels(['${:,.0f}'.format(x) for x in vals])
+    vals = ax2.get_yticks()
+    ax2.set_yticklabels(['${:,.0f}'.format(x) for x in vals]) 
     i = 0
     for x, y in sales.items():
-        ax.text(x+0.2, y, '${:,.0f}'.format(y), horizontalalignment='left', size='medium', color='black', weight='bold')
+        ax2.text(x+0.2, y, '${:,.0f}'.format(y), horizontalalignment='left', size='medium', color='black', weight='bold')
         i = i + 1
     for x, y in profits.items():
-        ax.text(x+0.2, y, '${:,.0f}'.format(y), horizontalalignment='left', size='medium', color='black', weight='bold')
+        ax2.text(x+0.2, y, '${:,.0f}'.format(y), horizontalalignment='left', size='medium', color='black', weight='bold')
         i = i + 1
     plt.show()
+    
+def chart_example_five():
+    xl = pd.ExcelFile("SalesDataFull.xlsx")
+    df = xl.parse("Orders")
+    df = _filter_df_by_date(df, "Order Date", 1, 2015, 1, 2015)
+    
+    region_central_df = df[(df['Region'] == 'Central')]
+    region_east_df = df[(df['Region'] == 'East')]
+    region_south_df = df[(df['Region'] == 'South')]
+    region_west_df = df[(df['Region'] == 'West')]
+    
+    filtered_region_central_df1 = region_central_df[['Order Date', 'Category', 'Discount']]
+    filtered_region_east_df1 = region_east_df[['Order Date', 'Category', 'Discount']]
+    filtered_region_south_df1 = region_south_df[['Order Date', 'Category', 'Discount']]
+    filtered_region_west_df1 = region_west_df[['Order Date', 'Category', 'Discount']]
+    
+    filtered_region_central_df2 = region_central_df[['Order Date', 'Category', 'Sub-Category', 'Discount']]
+    filtered_region_east_df2 = region_east_df[['Order Date', 'Category', 'Sub-Category', 'Discount']]
+    filtered_region_south_df2 = region_south_df[['Order Date', 'Category', 'Sub-Category', 'Discount']]
+    filtered_region_west_df2 = region_west_df[['Order Date', 'Category', 'Sub-Category', 'Discount']]
+    
+    fig, axs = plt.subplots(2, 4, figsize=(19,10))
+    
+    filtered_region_central_df1.groupby(['Category', 'Discount']).size().groupby(level=0).apply(
+        lambda x: 100 * x / x.sum()
+    ).unstack().plot(kind='bar', stacked=True, ax=axs[0,0])
+    axs[0,0].set_title('Central Region')
+    axs[0,0].set_ylabel('Percentage of Orders')
+    
+    filtered_region_east_df1.groupby(['Category', 'Discount']).size().groupby(level=0).apply(
+        lambda x: 100 * x / x.sum()
+    ).unstack().plot(kind='bar', stacked=True, ax=axs[0,1])
+    axs[0,1].set_title('East Region')
+    
+    filtered_region_south_df1.groupby(['Category', 'Discount']).size().groupby(level=0).apply(
+        lambda x: 100 * x / x.sum()
+    ).unstack().plot(kind='bar', stacked=True, ax=axs[0,2])
+    axs[0,2].set_title('South Region')
+    
+    filtered_region_west_df1.groupby(['Category', 'Discount']).size().groupby(level=0).apply(
+        lambda x: 100 * x / x.sum()
+    ).unstack().plot(kind='bar', stacked=True, ax=axs[0,3])
+    axs[0,3].set_title('West Region')
+    
+    filtered_region_central_df2.groupby(['Sub-Category', 'Discount']).size().groupby(level=0).apply(
+        lambda x: 100 * x / x.sum()
+    ).unstack().plot(kind='bar', stacked=True, ax=axs[1,0])
+    axs[1,0].set_title('Central Region')
+    axs[1,0].set_ylabel('Percentage of Orders')
+    
+    filtered_region_east_df2.groupby(['Sub-Category', 'Discount']).size().groupby(level=0).apply(
+        lambda x: 100 * x / x.sum()
+    ).unstack().plot(kind='bar', stacked=True, ax=axs[1,1])
+    axs[1,1].set_title('East Region')
+    
+    filtered_region_south_df2.groupby(['Sub-Category', 'Discount']).size().groupby(level=0).apply(
+        lambda x: 100 * x / x.sum()
+    ).unstack().plot(kind='bar', stacked=True, ax=axs[1,2])
+    axs[1,2].set_title('South Region')
+    
+    filtered_region_west_df2.groupby(['Sub-Category', 'Discount']).size().groupby(level=0).apply(
+        lambda x: 100 * x / x.sum()
+    ).unstack().plot(kind='bar', stacked=True, ax=axs[1,3])
+    axs[1,3].set_title('West Region')
+    
+    for x in range(0, 2):
+        for y in range (0, 4):
+            vals = axs[x, y].get_yticks()
+            axs[x, y].set_yticklabels(['{:.0f}%'.format(z) for z in vals])
+            current_handles, labels = axs[x, y].get_legend_handles_labels()
+            reversed_handles = reversed(current_handles)
+            for a in range(0, len(labels)):
+                labels[a] = '{:.0f}%'.format(float(labels[a]) * 100)
+            labels = reversed(labels)
+            axs[x, y].legend(reversed_handles, labels, loc='lower right', title="Discount")
+    
 
-chart_example_one()
-chart_example_two()
-chart_example_three()
+    plt.suptitle('Discounts by Product Category and Sub-Category by Region - Month: 1 Year: 2015')
+    plt.subplots_adjust(wspace=0.2, hspace=0.6)
+    plt.show()
+
+#chart_example_one()
+#chart_example_two()
+#chart_example_three()
+#chart_example_four()
+#chart_example_five()
+    
 chart_example_four()
+chart_example_three()
+chart_example_one()
+chart_example_five()
